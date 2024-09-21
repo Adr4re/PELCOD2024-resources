602d8b15134ff886a3ed08542e1aeba2c180bed2
adrianferdinand <adrianferdinand540@gmail.com>
2024-09-21 15:15:29 +0700

    Latihan Coding 2 tanggal 21 Sepetember 2024

---
+++ main.py
@@ -0,0 +1,12 @@
+# nilai = int(input("Masukka nilai: "))
+
+# if (nilai > 90 and nilai <= 100):
+#     print("Selamat anda lulus, dengan nilai yang memuaskan")
+# elif (nilai > 80):
+#     print("Selamat anda lulus")
+# else:
+#     print("anda masih belum lulus tes, semangat dan coba lagi")
+
+    #for loop
+for i in range (1, 10, 2):
+        print(i)
